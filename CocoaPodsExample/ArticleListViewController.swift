593a03e81de76984ffcb48fa3b34047673179d7a
Yoshio HANAWA <y@hnw.jp>
2016-06-12 21:43:55 +0900

    Implement Qiita viewer with Alamofire and SwiftyJSON

--- CocoaPodsExample/ArticleListViewController.swift
+++ CocoaPodsExample/ArticleListViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
-class ArticleListViewController: UIViewController {
+class ArticleListViewController: UIViewController, UITableViewDataSource {
     let table = UITableView()
+    var articles: [[String: String?]] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,6 +20,39 @@ class ArticleListViewController: UIViewController {
 
         table.frame = view.frame
         view.addSubview(table)
+        table.dataSource = self
+
+        getArticles()
+    }
+
+    func getArticles() {
+        Alamofire.request(.GET, "https://qiita.com/api/v2/items")
+            .responseJSON { response in
+                guard let object = response.result.value else {
+                    return
+                }
+                let json = JSON(object)
+                json.forEach({ (_, json) in
+                    let article: [String: String?] = [
+                        "title": json["title"].string,
+                        "userId": json["user"]["id"].string
+                    ]
+                    self.articles.append(article)
+                })
+                self.table.reloadData()
+        }
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return articles.count
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .Subtitle, reuseIdentifier: "cell")
+        let article = articles[indexPath.row]
+        cell.textLabel?.text = article["title"]!
+        cell.detailTextLabel?.text = article["userId"]!
+        return cell
     }
 }
 
